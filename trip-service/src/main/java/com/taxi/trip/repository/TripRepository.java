8d4b5d4f41baf8335f29a14d8ea2c8af883a3884
gle6chik <glebbaranov21@gmail.com>
2026-05-05 21:01:38 +0700

    feat: add trip repository

---
+++ trip-service/src/main/java/com/taxi/trip/repository/TripRepository.java
@@ -0,0 +1,10 @@
+package com.taxi.trip.repository;
+
+import com.taxi.trip.model.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+public interface TripRepository extends JpaRepository<Trip, Long> {
+    List<Trip> findByPassengerId(Long passengerId);
+}
