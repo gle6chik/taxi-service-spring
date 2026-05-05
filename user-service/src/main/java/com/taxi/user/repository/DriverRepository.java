61041327a9d550e1257d18f69fea399dd37befb6
gle6chik <glebbaranov21@gmail.com>
2026-05-05 12:05:17 +0700

    feat: add driver repository

---
+++ user-service/src/main/java/com/taxi/user/repository/DriverRepository.java
@@ -0,0 +1,9 @@
+package com.taxi.user.repository;
+
+import com.taxi.user.model.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+}
